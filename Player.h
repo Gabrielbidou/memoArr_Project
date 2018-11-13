45291ccc55f6b4339f5b9617c9baaab61679eda4
Gabriel Brunet <gbrun058@uottawa.ca>
2018-11-12 20:32:31 -0500

    Started working on Card

--- Player.h
+++ Player.h
@@ -1,2 +1,23 @@
 #pragma once
+#include <string>
 #include "Reward.h"
+
+using namespace std;
+class Player {
+private:
+	bool active;
+
+public:
+	const string name;
+	string side;
+	int numRubis;
+
+	Player(string name, string side, int numRubis) : name(name), side(side), numRubis(numRubis) {};
+
+	string getName() const { return name; }
+	void setActive(bool flag) { active = flag; }
+	bool isActive() { return active; }
+	int getNRubis() { return numRubis; }
+	void addReward(const Reward &reward);
+	void setDisplayMode(bool endOfGame);
+};
