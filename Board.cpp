d13924a419a346d25b6ffa17fac36202afc1f6d6
Gabriel Brunet <gbrun058@uottawa.ca>
2018-11-18 13:45:06 -0500

    Updated Class Board

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,106 @@
-#include <iostream>
+#include "pch.h"
+#include "Board.h"
+
+//Constructeur par défaut
+Board::Board()
+{
+	//Initialiser la Board comme étant vide
+	for (int i = 0; i < 20; i++) {
+		string s(20, ' ');
+		display[i] = s;
+	}
+
+	//Ajouter les lettres de la première colonne
+	display[1][0] = A; display[5][0] = B; display[9][0] = C; display[13][0] = D; display[17][0] = E;
+
+	//Ajouter les chiffres de la dernière rangée
+	display[19][2] = one; display[19][6] = two; display[19][10] = three; display[19][14] = four; display[19][18] = five;
+
+	//Ajouter tous les z pour les cartes face cachée
+	for (int i = 0; i < 20; i += 4) {
+		for (int j = 0; j < 3; j++) {
+			for (int k = 1; k < 20; k += 4) {
+				display[i + j].replace(k, 2, "zzz");
+			}
+		}
+	}
+
+	//Initialiser toutes les faces cachées
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			faceStatus[i][j] = false;
+}
+
+const bool Board::isFaceUp(const Letter & letter, const Number & number)
+{
+	try
+	{
+		getCardRange(letter, number);
+		return faceStatus[letter][number];
+	}
+	catch (const std::out_of_range &error)
+	{
+		
+	}
+}
+
+bool Board::turnFaceUp(const Letter & letter, const Number & number)
+{
+	return false;
+}
+
+bool Board::turnFaceDown(const Letter & letter, const Number & number)
+{
+	return false;
+}
+
+Card * Board::getCard(const Letter & letter, const Number & number)
+{
+	try
+	{
+		getCardRange(letter, number);
+		return cards[letter][number];
+	}
+	catch (const std::out_of_range &error)
+	{
+		throw(error);
+	}
+}
+
+void Board::setCard(const Letter & letter, const Number & number, Card * card)
+{
+	try
+	{
+		getCardRange(letter, number);
+		cards[letter][number] = card;
+	}
+	catch (const std::out_of_range &error)
+	{
+		throw(error);
+	}
+}
+
+void Board::reset()
+{
+}
+
+//Méthodes privées
+void Board::getCardRange(const Letter & letter, const Number & number)
+{
+	if (letter == C && number == three) {
+		out_of_range error();
+		cout << "This space is unusable";
+		throw(error);
+	}
+	if (letter < 0 || letter > 4 || number < 0 || number > 4) {
+		out_of_range error();
+		throw(error);
+	}
+}
+
+void Board::print()
+{
+	for (int j = 0; j < 20; j++) {
+		cout << display[j] << endl;
+	}
+}
--- Board.h
+++ Board.h
@@ -1,42 +1,39 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "Card.h"
 
 using namespace std;
 
-class Board {
-	typedef enum { A = (char) 'A', B = (char) 'B', C = (char) 'C', D = (char) 'D', E = (char) 'E' } Letter;
-	typedef enum { one = (char) '1', two = (char) '2', three = (char) '3', four = (char) '4', five = (char) '5' } Number;
+typedef enum { A = (char) 'A', B = (char) 'B', C = (char) 'C', D = (char) 'D', E = (char) 'E' } Letter;
+typedef enum { one = (char) '1', two = (char) '2', three = (char) '3', four = (char) '4', five = (char) '5' } Number;
 
+class Board {
+private:
+	Card *cards[5][5];
+	bool faceStatus[5][5];
 	char letters[5] = { 'A', 'B', 'C', 'D', 'E' };
 
 public:
 	string display[20];
 
-	Board() {
-		//Initialiser la Board comme étant vide
-		for (int i = 0; i < 20; i++) {
-			string s(20, ' ');
-			display[i] = s;
-		}
+	Board();
 
-		//Ajouter les lettres de la première colonne
-		display[1][0] = A; display[5][0] = B; display[9][0] = C; display[13][0] = D; display[17][0] = E;
-
-		//Ajouter les chiffres de la dernière rangée
-		display[19][2] = one; display[19][6] = two; display[19][10] = three; display[19][14] = four; display[19][18] = five;
-
-		//Ajouter tous les z pour les cartes face cachée
-		for (int i = 0; i < 20; i += 4) {
-			for (int j = 0; j < 3; j++) {
-				for (int k = 1; k < 20; k += 4) {
-					display[i + j].replace(k, 2, "zzz");
-				}
-			}
-		}
-
+	//Overload de l'opérateur <<
+	friend ostream& operator<<(ostream& os, Board& board)
+	{
+		board.print();
+		return os;
 	}
+
+	const bool isFaceUp(const Letter &letter, const Number &number);
+	bool turnFaceUp(const Letter &letter, const Number &number);
+	bool turnFaceDown(const Letter &letter, const Number &number);
+	Card* getCard(const Letter &letter, const Number &number);
+	void setCard(const Letter &letter, const Number &number, Card* card);
 	void reset();
-private: void print() {
-	for (int i = 0; i < 20; i++)
-		cout << display[i] << endl;
-}
+
+private:
+	void getCardRange(const Letter &letter, const Number &number);
+	void print();
 };
