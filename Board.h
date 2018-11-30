38b03770f497e4ad3bc30dc774a660e12f56fbee
Gabriel Brunet <gbrun058@uottawa.ca>
2018-11-30 18:40:05 -0500

    Finished Expert Display Mode

--- Board.h
+++ Board.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <vector>
 #include <string>
 #include "Deck.h"
 
@@ -33,74 +32,8 @@ public:
 	Card* getCard(const Letter &letter, const Number &number);
 	void setCard(const Letter &letter, const Number &number, Card* card);
 	virtual void reset();
-	void updateScreen();
+	virtual void updateScreen();
 protected:
 	void getCardRange(const Letter &letter, const Number &number);
-	
 	virtual void print();
-};
-
-class ExpertBoard : public Board {
-	char lettersChar[5] = { 'A', 'B', 'C', 'D', 'E' };
-	char numbersChar[5] = { '1', '2', '3', '4', '5' };
-	string display[4];
-	vector<string> cardsPosition;
-	vector<Card *> cardsVector;
-public:
-	ExpertBoard() {
-		for (int i = 0; i < 4; i++) {
-			string s(100, ' ');
-			display[i] = s;
-		}
-	}
-	virtual bool turnFaceUp(const Letter &letter, const Number &number) {
-		//Vérifie si la carte est déjà face dévoilée
-		if (isFaceUp(letter, number)) {
-			return false;
-		}
-
-		else {
-			string s(2, ' ');
-			s[0] = lettersChar[letter];
-			s[1] = numbersChar[number];
-			cardsPosition.push_back(s);
-			cardsVector.push_back(cards[letter][number]);
-			faceStatus[letter][number] = true;
-			return true;
-		}
-	}
-	virtual bool turnFaceDown(const Letter &letter, const Number &number) {
-		//Vérifie si la carte est déjà cachée
-		if (!isFaceUp(letter, number)) {
-			return false;
-		}
-
-		else {
-			cardsVector.pop_back();
-			faceStatus[letter][number] = false;
-			return true;
-		}
-	}
-	virtual void reset() {
-		//Configure toutes les cartes comme étant face cachée
-		for (int i = 0; i < 5; i++)
-			for (int j = 0; j < 5; j++)
-				faceStatus[i][j] = false;
-
-		for (int i = 0; i < cardsVector.size(); i++)
-			cardsVector.pop_back();
-	}
-private:
-	virtual void print() {
-		for (int i = 0; i < cardsVector.size(); i++) {
-			int displayPosition = i * 4;
-
-			for (int j = 0; j < 3; j++) {
-				Card &currentCard = *cardsVector[i];
-				display[j].replace(displayPosition, 3, currentCard(j));
-			}
-
-			display[4].replace(displayPosition, 2, cardsPosition[i]);
-		}
-	}
 };
